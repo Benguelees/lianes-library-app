5bf17b1e8d4a5b756dfd175140329a51b61bd3be
Bengü <bengiaknergiz@gmail.com>
2026-04-03 14:30:09 +0200

    Clean project files

--- db_read.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, text
-
-engine = create_engine("sqlite:///liane_library.db")
-
-with engine.connect() as connection:
-    result = connection.execute(text("SELECT * FROM books"))
-
-    for row in result:
-        print(row)
